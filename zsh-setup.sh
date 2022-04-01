07578a022b53706ea7090ef59ea0e0bf8d4b2ac2
hariprasadp <shreeyadav676@gmail.com>
2022-04-01 15:16:29 +0530

    Initial Commit

---
+++ zsh-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+sudo apt update
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cp -r --verbose ./zshrc ~/.zshrc
+echo " ================= Welcome to Zsh ==================== "
+cat ./welcome.txt
+echo " You can set Zsh as your defult shell by typing excecuting ==> chsh -s /usr/bin/zsh "
+zsh
